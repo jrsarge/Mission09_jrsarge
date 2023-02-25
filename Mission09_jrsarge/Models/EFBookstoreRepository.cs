6043478756de243c75595515b5e88a10102d46cd
Jacob Sargent <jrsarge@byu.edu>
2023-02-24 20:01:21 -0700

    mission 9 pt1 vid 8

---
+++ Mission09_jrsarge/Models/EFBookstoreRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_jrsarge.Models
+{
+    public class EFBookstoreRepository : IBookstoreRepository
+    {
+        public IQueryable<Book> Books { get; }
+    }
+}
