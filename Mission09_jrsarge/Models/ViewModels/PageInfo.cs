1ac90a730e88efdcde8be6a36632b2d7c875c3b1
Jacob Sargent <jrsarge@byu.edu>
2023-02-27 19:54:53 -0700

    mission 9 pt2 vid 5

---
+++ Mission09_jrsarge/Models/ViewModels/PageInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_jrsarge.Models.ViewModels
+{
+    public class PageInfo
+    {
+        public int TotalNumBooks { get; set; }
+        public int BooksPerPage { get; set; }
+        public int CurrentPage { get; set; }
+
+        //Figure out how many pages we need
+        //Cast totalNumBooks to a double and cast all those calculations to an int
+        public int TotalPages => (int) Math.Ceiling((double)TotalNumBooks / BooksPerPage);
+    }
+}
