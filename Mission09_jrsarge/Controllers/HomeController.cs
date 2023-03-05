9a5d14822bd14c879d10c9f3bf034b6504192784
Jacob Sargent <jrsarge@byu.edu>
2023-03-04 17:04:32 -0700

    mission 10 pt1 vid 6

--- Mission09_jrsarge/Controllers/HomeController.cs
+++ Mission09_jrsarge/Controllers/HomeController.cs
@@ -18,13 +18,14 @@ namespace Mission09_jrsarge.Controllers
             repo = temp;
         }
 
-        public IActionResult Index(int pageNum = 1)
+        public IActionResult Index(string category, int pageNum = 1)
         {
             int pageSize = 10;
 
             var x = new BooksViewModel
             {
                 Books = repo.Books
+                .Where(b => b.Category == category)
                 .OrderBy(b => b.Title)
                 .Skip((pageNum - 1) * pageSize)
                 .Take(pageSize),
