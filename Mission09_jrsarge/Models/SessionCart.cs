1d506dba9a153207110abd765408fa5f334a912c
Jacob Sargent <jrsarge@byu.edu>
2023-03-13 19:30:40 -0600

    mission 11 pt1 vid 1

---
+++ Mission09_jrsarge/Models/SessionCart.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Mission09_jrsarge.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Mission09_jrsarge.Models
+{
+    public class SessionCart : basket
+    {
+        [JsonIgnore]
+        public ISession Session { get; set; }
+
+        public override void AddItem(Book bookie, int qty)
+        {
+            base.AddItem(bookie, qty);
+            Session.SetJson("Cart", this);
+        }
+
+        public override void RemoveItem(Book bookie)
+        {
+            base.RemoveItem(bookie);
+            Session.SetJson("Cart", this);
+        }
+
+        public override void ClearCart()
+        {
+            base.ClearCart();
+            Session.Remove("Cart");
+        }
+
+    }
+}
