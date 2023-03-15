0da3e78b74b407c6a1f3ccc05b7dde2717676de6
Jacob Sargent <jrsarge@byu.edu>
2023-03-15 14:52:59 -0600

    mission 11 pt1 vid 6

--- Mission09_jrsarge/Pages/Cart.cshtml.cs
+++ Mission09_jrsarge/Pages/Cart.cshtml.cs
@@ -13,9 +13,10 @@ namespace Mission09_jrsarge.Pages
     {
         private IBookstoreRepository repo { get; set; }
 
-        public CartModel (IBookstoreRepository temp)
+        public CartModel (IBookstoreRepository temp, basket b)
         {
             repo = temp;
+            basket = b;
         }
 
         public basket basket { get; set; }
@@ -34,9 +35,11 @@ namespace Mission09_jrsarge.Pages
             return RedirectToPage(new { ReturnUrl = returnUrl });
         }
 
-        public IActionResult OnPostRemove()
+        public IActionResult OnPostRemove(int bookId, string returnUrl)
         {
+            basket.RemoveItem(basket.Items.First(x => x.Book.BookId == bookId).Book);
 
+            return RedirectToPage ( new {ReturnUrl = returnUrl});
         }
     }
 }
