5ef36ceeadafe8d9e282cc4ab544372962321d06
Jacob Sargent <jrsarge@byu.edu>
2023-03-15 17:48:45 -0600

    mission 11 pt2 vid 4

---
+++ Mission09_jrsarge/Controllers/CheckoutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_jrsarge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_jrsarge.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        public IActionResult RingOut()
+        {
+            return View(new Checkout());
+        }
+    }
+}
