79134cfae31866655aa9660b71e3f847762a72dd
Jacob Sargent <jrsarge@byu.edu>
2023-03-15 21:49:29 -0600

    mission 11 all done

---
+++ Mission09_jrsarge/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_jrsarge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_jrsarge.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private basket cart;
+        public CartSummaryViewComponent(basket cartService)
+        {
+            cart = cartService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
